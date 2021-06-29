89c7dcd7065c0aa03e4a77601136901554e61d62
Miguel Santamaria <msantamaria86@gmail.com>
2021-06-29 10:06:30 -0500

    seeds creadas

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,9 +1,10 @@
 class Service < ApplicationRecord
   belongs_to :user
+  has_many :appointments
 
   validates :price, presence: true
   validates :start_time, presence: true
   validates :duration, presence: true
-  validates :type, presence: true
+  # validates :type, presence: true
   validates :place, length: {minimum:5, maximum:40}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Appointment.destroy_all
+Service.destroy_all
+User.destroy_all
+
+user1 = User.new(email: "123@abc.com", password: "123456789", first_name: "Miguel", last_name: "Santamaria", phone_number: 123456789)
+user1.save
+
+user2 = User.new(email: "456@abc.com", password: "123456789", first_name: "Peter", last_name: "Clears", phone_number: 1234567890)
+user2.save
+
+service1 = Service.new(start_time: DateTime.now, duration: 1, date: Date.new, place: "calle falsa 123", price: 5, user: user1 )
+service1.save
+service2 = Service.new(start_time: DateTime.now, duration: 1, date: Date.new, place: "calle mas falsa 123", price: 6, user: user1 )  
+service2.save
+
+appointment1 = Appointment.new(service: service1, user: user2)
+appointment1.save
+appointment2 = Appointment.new(service: service2, user: user2)
+appointment2.save
