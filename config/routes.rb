bda3a8fd552eb89577d36a6fa65c522d4f4dfb25
Miguel Santamaria <msantamaria86@gmail.com>
2021-06-29 09:39:59 -0500

    rutas agregadas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :services do
+    resources :appointments, only: [:new, :create]
+  end
+
+  resources :appointments, except: [:new, :create]
 end
