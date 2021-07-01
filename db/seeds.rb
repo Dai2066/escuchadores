6e5a273a272bdc2611a5ff4c224301920e2477d8
Miguel Santamaria <msantamaria86@gmail.com>
2021-07-01 16:25:38 -0500

    seeds finales

--- db/seeds.rb
+++ db/seeds.rb
@@ -116,47 +116,47 @@ user9.save
 #CREATE SERVICES
 
 #iglesia
-service1 = Service.new(start_time: Faker::Time.forward(days: 5,  period: :evening, format: :long), duration: 1, place: "Buenos Aires", price: 700, user: user1, plan: "Soy teologo, me encanta hablar de temas espirituales. Tengo 50 años" )
+service1 = Service.new(start_time: Faker::Time.forward(days: 5,  period: :evening, format: :long), duration: 1, place: "Buenos Aires", price: 700, user: user1, plan: "Soy teologo, me encanta hablar de temas espirituales. Tengo 50 años y me considero muy paciente" )
 service1.photo.attach(io: file_service1, filename: 'test1.png', content_type: 'image/png')
 service1.save
 
 #caminar
-service2 = Service.new(start_time: Faker::Time.forward(days: 5,  period: :evening, format: :long), duration: 2, place: "Buenos Aires", price: 750, user: user2, plan: "Me gusta mucho estar al aire libre, largas conversaciones, hacer amigos" )
+service2 = Service.new(start_time: Faker::Time.forward(days: 5,  period: :evening, format: :long), duration: 2, place: "Buenos Aires", price: 750, user: user2, plan: "Me gusta mucho estar al aire libre, tomar caminatas, tener largas conversaciones y hacer amigos" )
 service2.photo.attach(io: file_service2, filename: 'test2.png', content_type: 'image/png')
 service2.save
 
 # #existir
- service3 = Service.new(start_time: Faker::Time.forward(days: 5,  period: :evening, format: :long), duration: 1, place: "Buenos Aires", price: 800, user: user3, plan: "Me encanta meditar, conectar con otros a nivel personal, padre de 2 hijos" )
+ service3 = Service.new(start_time: Faker::Time.forward(days: 5,  period: :evening, format: :long), duration: 1, place: "Buenos Aires", price: 800, user: user1, plan: "Me encanta meditar y sentirme en paz, conectar con otros a nivel personal, soy padre de 2 hijos" )
  service3.photo.attach(io: file_service3, filename: 'test2.png', content_type: 'image/png')
  service3.save
 
 # #pasear a tu perro
- service4 = Service.new(start_time: Faker::Time.forward(days: 5,  period: :evening, format: :long), duration: 1, place: "Mendoza", price: 950, user: user4, plan: "Me encantan los animales, soy veterinario desde hace mas de 10 años" )
+ service4 = Service.new(start_time: Faker::Time.forward(days: 5,  period: :evening, format: :long), duration: 1, place: "Mendoza", price: 950, user: user1, plan: "Me encantan los animales, tengo 2 gatos y un perro, soy veterinario desde hace mas de 10 años" )
  service4.photo.attach(io: file_service4, filename: 'test2.png', content_type: 'image/png')
  service4.save
 
 # #tomar café
- service5 = Service.new(start_time: Faker::Time.forward(days: 5,  period: :evening, format: :long), duration: 1, place: "Mendoza", price: 700, user: user5, plan: "Soy barista, disfruto mucho la compañia de otros, socializar, ir de fiesta.." )
+ service5 = Service.new(start_time: Faker::Time.forward(days: 5,  period: :evening, format: :long), duration: 1, place: "Mendoza", price: 700, user: user2, plan: "Soy barista, disfruto mucho la compañia de otros, socializar, ir de fiesta.." )
  service5.photo.attach(io: file_service5, filename: 'test2.png', content_type: 'image/png')
  service5.save
 
 # #cena con tu familia
- service6 = Service.new(start_time: Faker::Time.forward(days: 5,  period: :evening, format: :long), duration: 1, place: "Cordoba", price: 650, user: user6, plan: "Me desempeño como terapeuta hace muchos años, busco poder compartir con tu circulo social" )
+ service6 = Service.new(start_time: Faker::Time.forward(days: 5,  period: :evening, format: :long), duration: 1, place: "Cordoba", price: 650, user: user1, plan: "Me desempeño como terapeuta hace muchos años, busco poder compartir con tu circulo social" )
  service6.photo.attach(io: file_service6, filename: 'test2.png', content_type: 'image/png')
  service6.save
 
 # #correr
- service7 = Service.new(start_time: Faker::Time.forward(days: 5,  period: :evening, format: :long), duration: 2, place: "Cordoba", price: 850, user: user7, plan: "Amo el deporte! en especial correr y hacer trekking, prefiero practicar deporte con otros" )
+ service7 = Service.new(start_time: Faker::Time.forward(days: 5,  period: :evening, format: :long), duration: 2, place: "Cordoba", price: 850, user: user2, plan: "Amo el deporte! en especial correr y hacer trekking, prefiero practicar deporte con otros" )
  service7.photo.attach(io: file_service7, filename: 'test2.png', content_type: 'image/png')
  service7.save
 
 # #al museo
-service8 = Service.new(start_time: Faker::Time.forward(days: 5,  period: :evening, format: :long), duration: 2, place: "Cordoba", price: 800, user: user8, plan: "Mis cosas favoritas son la poesia, los libros, el arte. Prefiero hablar de temas culturales" )
+service8 = Service.new(start_time: Faker::Time.forward(days: 5,  period: :evening, format: :long), duration: 2, place: "Cordoba", price: 800, user: user1, plan: "Mis cosas favoritas son la poesia, los libros, el arte. Prefiero hablar de temas culturales" )
 service8.photo.attach(io: file_service8, filename: 'test2.png', content_type: 'image/png')
 service8.save
 
 # #a estudiar
- service9 = Service.new(start_time: Faker::Time.forward(days: 5,  period: :evening, format: :long), duration: 3, place: "Rosario", price: 1000, user: user9, plan: "Soy docente, trato de ayudar a que las personas aprenden de la forma mas facil posible" )
+ service9 = Service.new(start_time: Faker::Time.forward(days: 5,  period: :evening, format: :long), duration: 3, place: "Rosario", price: 1000, user: user2, plan: "Soy docente, trato de ayudar a que las personas aprenden de la forma mas facil posible" )
  service9.photo.attach(io: file_service9, filename: 'test2.png', content_type: 'image/png')
  service9.save
 
